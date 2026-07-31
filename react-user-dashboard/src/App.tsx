b288a6d86b7874ec49ab467171c15fb36bc269b4
Mike-Franco <mikefranco.24@ichat.sp.edu.sg>
2026-07-31 08:50:07 +0800

    Link participant search workflow (#56)
    Route participant search links to the existing secure participant workflow and discard the obsolete duplicate implementation.

--- react-user-dashboard/src/App.tsx
+++ react-user-dashboard/src/App.tsx
@@ -77,6 +77,7 @@ export default function App() {
 
         {/* Participant & Registration Workflows */}
         <Route path="/participants" element={<ParticipantSearchPage />} />
+        <Route path="/participants/search" element={<ParticipantSearchPage />} />
         <Route path="/participants/new" element={<ParticipantCreatePage />} />
         <Route path="/participants/:participantId" element={<ParticipantDetailPage />} />
         <Route path="/participants/:participantId/edit" element={<ParticipantEditPage />} />
